47681c196165dd0f9209a5057a72687dac69098a
파랑이 <parangee9706@gmail.com>
2020-10-28 13:34:03 +0000

    Feat: Active page display in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
 
-const Bar = () => (
+const Bar = ({location}) => (
     <Navbar collapseOnSelect style={{width:'100vw'}} bg = 'light' fixed = 'top' expand = 'lg'>
         <Navbar.Brand as={Link} to = '/'><img style = {{width: '50px', height: '50px', borderRadius: '25px'}} src = {`${process.env.PUBLIC_URL}/teamkat.png`} alt = 'teamkat logo' /><span className = 'brandname'>Team Kat</span></Navbar.Brand>
         <Navbar.Toggle aria-controls = 'responsive-navbar-nav' />
@@ -17,16 +17,16 @@ const Bar = () => (
                         link: '/recruit',
                         text: 'Recruit'
                     }].map(i => (
-                        <Nav.Link as={Link} to = {i.link}>{i.text}</Nav.Link>
+                        <Nav.Link as={Link} to = {i.link} active={location.pathname === i.link}>{i.text}</Nav.Link>
                     ))
                 }
             </Nav>
             <hr />
             <Nav>
-                <Nav.Link href = 'https://discord.gg/R5UG5mR'>Server</Nav.Link>
+                <Nav.Link  href = 'https://discord.gg/R5UG5mR'>Server</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
 )
 
-export default Bar
+export default withRouter(Bar)
