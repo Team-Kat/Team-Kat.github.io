e51b2635bffa343b28b7bdb2cb66d696ef1c3da5
kiki7000 <devkiki7000@gmail.com>
2020-11-25 20:35:52 +0900

    footer

---
+++ src/components/Main/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import image from '../../../public/img/teamkat.png'
+
+const StyledFooter = styled.div`
+    width: 100vw;
+    height: 30vh;
+
+    background-color: #3a9ad9;
+    color: white;
+
+    border-top-left-radius: 70%;
+    border-top-right-radius: 70%;
+
+    text-align: center;
+
+    .logo {
+        width: 120px;
+        height: 120px;
+
+        margin-top: 5vh;
+    }
+`
+
+const Footer = ({ name, discordID, desc, github, avatar }) => (
+    <StyledFooter>
+        <img className = 'logo' src = {image} />
+    </StyledFooter>
+)
+
+export default Footer
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Head from '../components/Main/Head'
 import About from '../components/Main/About'
 import Members from '../components/Main/Members'
+import Footer from '../components/Main/Footer'
 
 const Main = () => (
     <>
         <Head />
         <About />
         <Members />
+        <Footer />
     </>
 )
 
