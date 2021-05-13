4ed3f080cf5c1466e46d4234e792c1fb31eb4d4e
kiki7000 <devkiki7000@gmail.com>
2021-05-13 13:33:45 +0900

    refactor: add ifs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './styles/global.css'
 import 'aos/dist/aos.css';
 import 'reactjs-popup/dist/index.css';
 
-import { init, refresh }  from 'aos'
+import { init, refresh } from 'aos'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Main from './routes/Main'
@@ -16,15 +16,15 @@ const App = () => {
         refresh()
     }, [])
 
-    if (window.innerWidth < 350) alert('스마트폰 화면에서 컨테이너가 겹치는 현상이 있습니다. 현재 고치는 중입니다.')
-    if (window.innerHeight < 350) alert('너무 세로 길이가 짧아서 화면이 재대로 보이지 않을수 있습니다')
+    if (window.innerWidth < 350 || window.innerHeight < 350)
+        alert('가로, 세로 350px 미만 기기는 지원하지 않습니다.');
 
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path = '/' component = {Main} />
-                <Route exact path = '/privacy' component = {Privacy} />
-                <Route component = {NotFound} />
+                <Route exact path='/' component={Main} />
+                <Route exact path='/privacy' component={Privacy} />
+                <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
     )
